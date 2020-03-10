4c9bfe70fc7327c5109a3709b8c34ddf45754d65
Christina Machak <christina@takescoop.com>
2020-03-09 20:30:14 -0700

    refactor grid to dictionary from array

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
+import numpy as np
 import random
 
 # The game will be played in a 3x3 grid
 ROWS = 3
-COLS = 3
+COLS = ROWS
 
-# instead of using array of array just use a dictionary
 
 class Player:
     """
@@ -20,40 +20,55 @@ class Player:
         self.badge = badge
 
 
+class Grid:
+
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.ord_a = 97  # integer corresponding to the unicode character 'a'
+        self.row_indices = [chr(i) for i in range(self.ord_a, self.ord_a + rows)]
+        self.col_indices = [str(i) for i in range(1, cols + 1)]
+        self.grid_keys = list(zip(list(np.repeat([chr(i) for i in range(self.ord_a, self.ord_a + rows)], cols)),
+                                  [str(i) for i in range(1, cols + 1)] * rows))
+        self.grid = {key: ' ' for key in self.grid_keys}
+
+    def print_grid(self):
+        """Print the current game grid including the location of past players' moves."""
+        print(f"    1   2   3 ")
+        print(f"    .   .   . ")
+        print(f"a . {self.grid['a', '1']} | {self.grid['a', '2']} | {self.grid['a', '3']} ")
+        print(f"   ---|---|---")
+        print(f"b . {self.grid['b', '1']} | {self.grid['b', '2']} | {self.grid['b', '3']} ")
+        print(f"   ---|---|---")
+        print(f"c . {self.grid['c', '1']} | {self.grid['c', '2']} | {self.grid['c', '3']} ")
+
+
 class Game:
     """
-    Hold state of the tic-tac-toe game.
+    Hold state of the tic-tac-toe game, the grid, and the players.
 
     Attributes:
-         grid (array[array[badge]]) :
+         grid dict{k: Badge}:
          players (array[Player]):
-         currentPlayer (Player):
+         current_player (Player):
          gridKey (dict) :
          movesRemaining (int): Number of moves remaining before the grid is full.
     """
 
     def __init__(self):
-        self.grid = [[' ' for i in range(COLS)] for j in range(ROWS)]
-        self.players = self.startGame()
-        self.currentPlayer = random.choice(self.players)
-        self.gridKey = {"a": 0, "b": 1, "c": 2}
-        self.movesRemaining = ROWS*COLS
+        self.grid = Grid(ROWS, COLS)
+        self.players = []
+        self.current_player = None
+        self.game_over = False
 
-    def print_grid(self):
-        """Print the current game grid including the location of past players' moves."""
-        print(f'    0   1   2 ')
-        print(f'    .   .   . ')
-        print(f'a . {self.grid[0][0]} | {self.grid[0][1]} | {self.grid[0][2]} ')
-        print(f'   ---|---|---')
-        print(f'b . {self.grid[1][0]} | {self.grid[1][1]} | {self.grid[1][2]} ')
-        print(f'   ---|---|---')
-        print(f'c . {self.grid[2][0]} | {self.grid[2][1]} | {self.grid[2][2]} ')
-
-    def startGame(self):
+    def start_game(self):
         """Start the game by printing prompts for user to input player names,
         prints the empty game board grid and instructions."""
         player1 = Player(input("Who is the first player? "), 'X')
         player2 = Player(input("Who is the second player? "), 'O')
+
+        self.players = [player1, player2]
+        self.current_player = random.choice(self.players)
 
         print("-----------------------------------------------------------------------------------------------")
         print(f"Welcome to the game {player1.name} and {player2.name}, good luck!")
@@ -61,22 +76,28 @@ class Game:
         print("To make a move enter a letter followed by a number to indicate the square you want to play in.")
         print("-----------------------------------------------------------------------------------------------")
 
-        self.print_grid()
-        return [player1, player2]
+        self.grid.print_grid()
 
-    def __is_valid_turn(self, letter, number):
-        """Determine if a user's input results in a valid turn.
-        Args:
-            letter (str):
-            number (str):
+    def __is_valid_turn(self, move):
         """
-        if letter not in ('a', 'b', 'c'):
+        Determine ig a user's input results in a valid turn.
+        :param move:
+        :return:
+        """
+        # the user entered nothing
+        if move == '':
             return False
-        if number not in ('0', '1', '2'):
+
+        # the letter input is not one of the row indices
+        if move[0] not in self.grid.row_indices:
             return False
-        r = self.gridKey[letter]
-        c = int(number)
-        if (self.grid[r][c] == 'X') | (self.grid[r][c] == 'O'):
+
+        # the number input is not one of the column indices
+        if move[1] not in self.grid.col_indices:
+            return False
+
+        # the location entered has already been marked in a previous turn
+        if (self.grid.grid[(move[0], move[1])] == 'X') | (self.grid.grid[(move[0], move[1])] == 'O'):
             return False
 
         return True
@@ -87,82 +108,62 @@ class Game:
         :param try_again:
         :return:
         """
-        print(' ')
         if try_again:
-            move = input(f'That is not a valid move {self.currentPlayer.name}, try again.')
+            move = input(f'Invalid move {self.current_player.name}. Try again :')
         else:
-            move = input(f"Make your move  {self.currentPlayer.name}, (enter letter then number, example a1): ")
+            move = input(f"Make your move {self.current_player.name} : ")
 
-        letter = move[0]
-        number = move[1]
-
-        if self.__is_valid_turn(letter, number):
-            self.__update_grid(letter,number)
-            self.print_grid()
+        if self.__is_valid_turn(move):
+            self.grid.grid[(move[0], move[1])] = self.current_player.badge
+            self.grid.print_grid()
+            self.__check_state()
             self.__switch_player()
-            self.movesRemaining = self.movesRemaining - 1
-            self.check_state()
         else:
-            print()
-            self.take_turn(True)
+            self.take_turn(try_again=True)
 
     def __switch_player(self):
-        """Switch from one player as current player to the other."""
-        if self.currentPlayer == self.players[0]:
-            self.currentPlayer = self.players[1]
+        """
+        Switch from one player as current player to the other.
+        :return: None
+        """
+        if self.current_player == self.players[0]:
+            self.current_player = self.players[1]
         else:
-            self.currentPlayer = self.players[0]
+            self.current_player = self.players[0]
 
-    def __declare_game_over(self, msg):
-        """Print message declaring game's end and set state of gameOver.
-        Args:
-            msg (str): Specific message to print following Game Over announcement.
-        """
-        print(f'Game Over! {msg}')
+    def __check_for_win(self, ind_keys):
+        if all([self.grid.grid[(x, y)] == self.grid.grid[ind_keys[0]] for (x, y) in ind_keys]) \
+                and self.grid.grid[ind_keys[0]] != ' ':
+            print(f'Game Over! The winner is {self.current_player.name}!')
+            self.game_over = True
 
-    def __update_grid(self, letter, number):
-        """Update game grid with most recent valid move.
-        Args:
-            letter (str): The character denoting the row of the player's move.
-            number (str): The character representation of the number denoting the column
-                          of the player's move.
-        """
-        r = self.gridKey[letter]
-        c = int(number)
-        self.grid[r][c] = self.currentPlayer.badge
+    def __check_state(self):
 
-    def check_state(self):
-        """Check if the game has ended after last played turn due to win or tie."""
+        # check for tie
+        if len([badge for badge in self.grid.grid.values() if badge == ' ']) == 0:
+            print(f'Game Over! No moves remain, it is a tie!')
+            self.game_over = True
 
-        # check if the game has ended in a tie
-        if self.movesRemaining == 0:
-            return self.__declare_game_over('It\'s a Tie! No more moves remain.')
+        # check for a horizontal win
+        for ind in self.grid.row_indices:
+            self.__check_for_win(list(zip([ind]*self.grid.cols, self.grid.col_indices)))
 
-        # check for all the horizontal wins
-        for r in range(ROWS):
-            if self.grid[r][0] == ' ':
-                continue
-            if all([x == self.grid[r][0] for x in [self.grid[r][1], self.grid[r][2]]]):
-                print(all([x == self.grid[r][0] for x in [self.grid[r][1], self.grid[r][2]]]))
-                return self.__declare_game_over(f'The winner is {self.currentPlayer.name}!')
+        # check for a vertical win
+        for ind in self.grid.col_indices:
+            self.__check_for_win(list(zip(self.grid.row_indices, [ind]*self.grid.rows)))
 
-        # check for all the vertical wins
-        for c in range(COLS):
-            if self.grid[0][c] == ' ':
-                continue
-            if all([x == self.grid[0][c] for x in [self.grid[1][c], self.grid[2][c]]]):
-                return self.__declare_game_over(f'The winner is {self.currentPlayer.name}!')
+        # check for a left to right diagonal win
+        self.__check_for_win(list(zip(self.grid.row_indices, self.grid.col_indices)))
 
-        # check for the diagonal wins
-        if all([x == self.grid[0][0] for x in [self.grid[1][1], self.grid[2][2]]]) & (self.grid[0][0] != ' '):
-            return self.__declare_game_over(f'The winner is {self.currentPlayer.name}!')
-
-        if all([x == self.grid[0][2] for x in [self.grid[1][1], self.grid[2][0]]]) & (self.grid[0][2] != ' '):
-            return self.__declare_game_over(f'The winner is {self.currentPlayer.name}!')
+        # check for a right to left diagonal win
+        self.__check_for_win(list(zip(self.grid.row_indices, self.grid.col_indices.__reversed__())))
 
 
 if __name__ == '__main__':
 
     aGame = Game()
-    while aGame.movesRemaining > 0:
+    aGame.start_game()
+
+    # while some spots on the game board are blank, keep taking turns
+    while not aGame.game_over:
         aGame.take_turn()
